0e6f4bda9f62664720a80107120c3c5bd08c739b
ALLAN DE SOUSA ALMEIDA <25203610@CFP127ALUNO.LOCAL>
2026-06-11 09:33:34 -0300

    adicionando JSON teste

---
+++ src/produto.js
@@ -0,0 +1,92 @@
+'use strict'
+
+export const listaProdutos = [
+    {
+      "id": 1,
+      "nome": "Brigadeiro Gourmet Belga",
+      "descricao": "Delicioso brigadeiro artesanal feito com chocolate belga 54% e finalizado com granulado split.",
+      "imagem": "brigadeiro-gourmet-belga.png",
+      "unidade_produto": 42,
+      "quantidade_sabores": 3
+    },
+    {
+      "id": 2,
+      "nome": "Pudim de Leite Condensado",
+      "descricao": "O clássico super cremoso, sem furinhos, com uma calda de caramelo artesanal perfeita.",
+      "imagem": "pudim-leite-condensado.png",
+      "unidade_produto": 15,
+      "quantidade_sabores": 1
+    },
+    {
+      "id": 3,
+      "nome": "Beijinho de Coco Defumado",
+      "descricao": "Doce tradicional de festa feito com leite condensado e coco ralado fresco tostado.",
+      "imagem": "beijinho-coco.png",
+      "unidade_produto": 88,
+      "quantidade_sabores": 2
+    },
+    {
+      "id": 4,
+      "nome": "Coxinha de Morango com Brigadeiro",
+      "descricao": "Morango fresco e inteiro envolvido por uma camada generosa de brigadeiro tradicional.",
+      "imagem": "coxinha-morango-brigadeiro.png",
+      "unidade_produto": 23,
+      "quantidade_sabores": 2
+    },
+    {
+      "id": 5,
+      "nome": "Brownie com Recheio de Nutella",
+      "descricao": "Brownie denso e molhadinho por dentro, com casquinha crocante e muita Nutella.",
+      "imagem": "brownie-nutella.png",
+      "unidade_produto": 67,
+      "quantidade_sabores": 4
+    },
+    {
+      "id": 6,
+      "nome": "Palha Italiana Tradicional",
+      "descricao": "A mistura perfeita de brigadeiro cremoso com pedaços crocantes de biscoito maisena.",
+      "imagem": "palha-italiana.png",
+      "unidade_produto": 12,
+      "quantidade_sabores": 1
+    },
+    {
+      "id": 7,
+      "nome": "Quindim Premium",
+      "descricao": "Doce dourado à base de gemas, açúcar e coco ralado, com aquele brilho inconfundível.",
+      "imagem": "quindim-premium.png",
+      "unidade_produto": 94,
+      "quantidade_sabores": 1
+    },
+    {
+      "id": 8,
+      "nome": "Torta de Limão Siciliano",
+      "descricao": "Massa frola crocante, recheio cremoso de limão e cobertura de merengue suíço tostado.",
+      "imagem": "torta-limao-siciliano.png",
+      "unidade_produto": 5,
+      "quantidade_sabores": 3
+    },
+    {
+      "id": 9,
+      "nome": "Macaron de Frutas Vermelhas",
+      "descricao": "Clássico doce francês com casquinha leve e recheio de ganache de frutas vermelhas.",
+      "imagem": "macaron-frutas-vermelhas.png",
+      "unidade_produto": 31,
+      "quantidade_sabores": 5
+    },
+    {
+      "id": 10,
+      "nome": "Bolo de Pote Prestígio",
+      "descricao": "Camadas intercaladas de bolo de chocolate fofinho e recheio cremoso de coco.",
+      "imagem": "bolo-pote-prestigio.png",
+      "unidade_produto": 76,
+      "quantidade_sabores": 2
+    },
+    {
+      "id": 11,
+      "nome": "Trufa Artesanal de Maracujá",
+      "descricao": "Casquinha de chocolate nobre recheada com ganache azedinha de puro maracujá.",
+      "imagem": "trufa-maracuja.png",
+      "unidade_produto": 50,
+      "quantidade_sabores": 3
+    }
+  ]
